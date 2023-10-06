96ef9192efa67d1cfda6a9901dbb0d1c41dd83e6
mohamuhsin <muhsinohoho@outlook.com>
2023-10-06 16:26:31 +0300

    wrapped list with card

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Card.module.css";
 
 const Card = (props) => {
-  return <div className={classes.card}>{props.chidren}</div>;
+  return <div className={classes.card}>{props.children}</div>;
 };
 
 export default Card;
