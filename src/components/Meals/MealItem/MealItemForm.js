835101215c318769050cccae38638e6f8525a496
mohamuhsin <muhsinohoho@outlook.com>
2023-10-11 13:57:29 +0300

    working on mealitemform

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import classes from "./MealItemForm.module.css";
 
 const MealItemForm = () => {
   return (
@@ -8,3 +9,5 @@ const MealItemForm = () => {
     </form>
   );
 };
+
+export default MealItemForm;
