2d5d45e52ad1c71e65433cf2a6a9c904da4a2bda
mohamuhsin <muhsinohoho@outlook.com>
2023-10-12 12:52:15 +0300

    working on modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,3 +1,19 @@
-const Modal = (props) => {};
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  co;
+};
 
 export default Modal;
