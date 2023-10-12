dd96c33629a2b490fb7c391f4bd506b53a0e67db
mohamuhsin <muhsinohoho@outlook.com>
2023-10-12 12:38:44 +0300

    started working on cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,20 @@
 import classes from "./Cart.module.css";
 
-const Cart = (props) => {};
+const Cart = (props) => {
+  const cartItems = (
+    <ul>
+      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <div>
+      {cartItems}
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
 
 export default Cart;
