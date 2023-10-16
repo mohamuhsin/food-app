553d037bac9dfd4a6d5cebdbafc4947d360af3ec
mohamuhsin <muhsinohoho@outlook.com>
2023-10-16 14:44:13 +0300

    started working on cart context

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,7 +1,22 @@
 import CartContext from "./cart-context";
 
 const CartProvider = (props) => {
-  return <CartContext.Provider>{props.children}</CartContext.Provider>;
+  const addItemToCartHandler = (item) => {};
+
+  const removeItemFromCartHandler = (id) => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
